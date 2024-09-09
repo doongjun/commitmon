61dd14db90507b54b64b6ba02ba4cc3c6dcd523a
doongjun <dongjun.kim@purpleworks.co.kr>
2024-09-09 15:50:23 +0900

    first commit

---
+++ src/main/kotlin/com/doongjun/commitmon/CommitmonApplication.kt
@@ -0,0 +1,11 @@
+package com.doongjun.commitmon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommitmonApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommitmonApplication>(*args)
+}
