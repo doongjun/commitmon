fcf738812e0104ff7323e6ba43e02a303bbd7b55
doongjun <dongjun.kim@purpleworks.co.kr>
2024-09-23 16:51:46 +0900

    feat: 애니메이션 비즈니스 로직 수정

--- src/main/kotlin/com/doongjun/commitmon/app/AdventureFacade.kt
+++ src/main/kotlin/com/doongjun/commitmon/app/AdventureFacade.kt
@@ -1,6 +1,11 @@
 package com.doongjun.commitmon.app
 
+import com.doongjun.commitmon.app.data.CreateUserDto
+import com.doongjun.commitmon.app.data.GetUserDto
+import com.doongjun.commitmon.app.data.PatchUserDto
+import com.doongjun.commitmon.core.AdventureGenerator
 import org.springframework.stereotype.Component
+import org.thymeleaf.context.Context
 import org.thymeleaf.spring6.SpringTemplateEngine
 
 @Component
@@ -9,46 +14,52 @@ class AdventureFacade(
     private val githubService: GithubService,
     private val svgTemplateEngine: SpringTemplateEngine,
 ) {
-//    fun getCommitmonAdventure(username: String): String {
-//        val (githubId, totalCommitCount) = githubService.getUserCommitInfo(username)
-//        val (followerGithubIds, followingGithubIds) = githubService.getFollowInfo(username, 100)
-//
-//        val user =
-//            userService.createOrUpdate(
-//                CreateOrUpdateUserDto(
-//                    githubId = githubId,
-//                    name = username,
-//                    totalCommitCount = totalCommitCount,
-//                    followerGithubIds = followerGithubIds,
-//                    followingGithubIds = followingGithubIds,
-//                ),
-//            )
-//
-//        val templates =
-//            (user.mutualFollowers + user.toSimple()).joinToString { follower ->
-//                svgTemplateEngine.process(
-//                    "asset/${follower.commitmon.assetName}",
-//                    Context().apply {
-//                        setVariable("id", follower.id)
-//                        setVariable("motion", AdventureGenerator.generateMotion())
-//                        setVariable("y", AdventureGenerator.generateY(follower.commitmon.isFlying))
-//                        setVariable("username", follower.name)
-//                    },
-//                )
-//            }
-//
-//        val adventure =
-//            svgTemplateEngine.process(
-//                "adventure",
-//                Context().apply {
-//                    setVariable("commitmons", templates)
-//                },
-//            )
-//        return adventure
-//    }
-//
-//    fun getAdventure(username: String): String {
-//        if (userService.existsByName(username)) {
-//        }
-//    }
+    fun getAnimation(username: String): String {
+        val (githubId, totalCommitCount) = githubService.getUserCommitInfo(username)
+
+        return runCatching {
+            val user = userService.getByGithubId(githubId)
+            PatchUserDto(
+                name = username,
+                totalCommitCount = totalCommitCount,
+            ).let { dto ->
+                userService.patch(user.id, dto)
+            }
+            createAnimation(userService.get(user.id))
+        }.getOrElse { e ->
+            if (e is IllegalArgumentException) {
+                CreateUserDto(
+                    githubId = githubId,
+                    name = username,
+                    totalCommitCount = totalCommitCount,
+                ).let { dto ->
+                    createAnimation(userService.get(userService.create(dto)))
+                }
+            } else {
+                throw e
+            }
+        }
+    }
+
+    private fun createAnimation(user: GetUserDto): String {
+        val templates =
+            (user.mutualFollowers + user.toSimple()).joinToString { follower ->
+                svgTemplateEngine.process(
+                    "asset/${follower.commitmon.assetName}",
+                    Context().apply {
+                        setVariable("id", follower.id)
+                        setVariable("motion", AdventureGenerator.generateMotion())
+                        setVariable("y", AdventureGenerator.generateY(follower.commitmon.isFlying))
+                        setVariable("username", follower.name)
+                    },
+                )
+            }
+
+        return svgTemplateEngine.process(
+            "adventure",
+            Context().apply {
+                setVariable("templates", templates)
+            },
+        )
+    }
 }
