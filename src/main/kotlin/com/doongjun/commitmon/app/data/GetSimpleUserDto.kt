0f6a8f8287aa563e4edc162d55f2639f78dec560
doongjun <dongjun.kim@purpleworks.co.kr>
2024-09-24 11:17:04 +0900

    feat: Simple 유저 조회 service

---
+++ src/main/kotlin/com/doongjun/commitmon/app/data/GetSimpleUserDto.kt
@@ -0,0 +1,23 @@
+package com.doongjun.commitmon.app.data
+
+import com.doongjun.commitmon.domain.Commitmon
+import com.doongjun.commitmon.domain.User
+
+data class GetSimpleUserDto(
+    val id: Long,
+    val githubId: Long,
+    val name: String,
+    val totalCommitCount: Long,
+    val commitmon: Commitmon,
+) {
+    companion object {
+        fun from(user: User): GetSimpleUserDto =
+            GetSimpleUserDto(
+                id = user.id,
+                githubId = user.githubId,
+                name = user.name,
+                totalCommitCount = user.totalCommitCount,
+                commitmon = user.commitmon,
+            )
+    }
+}
