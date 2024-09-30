568110e84a8f057c89ac51d32aa615066c18cd51
doongjun <dongjun.kim@purpleworks.co.kr>
2024-09-30 16:27:15 +0900

    #23 adventure 엔드포인트 cache control

--- src/main/kotlin/com/doongjun/commitmon/api/AdventureController.kt
+++ src/main/kotlin/com/doongjun/commitmon/api/AdventureController.kt
@@ -3,6 +3,8 @@ package com.doongjun.commitmon.api
 import com.doongjun.commitmon.app.AdventureFacade
 import com.doongjun.commitmon.app.Theme
 import com.doongjun.commitmon.extension.findBy
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -15,9 +17,15 @@ class AdventureController(
     fun getAdventure(
         @RequestParam username: String,
         @RequestParam(required = false) theme: String?,
-    ): String =
-        adventureFacade.getAnimation(
+        response: HttpServletResponse,
+    ): String {
+        response.apply {
+            setHeader(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate, max-age=3600")
+            setHeader(HttpHeaders.PRAGMA, "no-cache")
+        }
+        return adventureFacade.getAnimation(
             username = username,
             theme = Theme::assetName.findBy(theme),
         )
+    }
 }
