7ff0381fbae3d81d516c0365688f214e2af11cef
doongjun <dongjun.kim@purpleworks.co.kr>
2024-11-18 11:29:31 +0900

    #50 config: 비동기 커스텀 설정

--- src/main/kotlin/com/doongjun/commitmon/config/AsyncConfig.kt
+++ src/main/kotlin/com/doongjun/commitmon/config/AsyncConfig.kt
@@ -1,8 +1,27 @@
 package com.doongjun.commitmon.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 
-@Configuration
 @EnableAsync
-class AsyncConfig
+@Configuration
+class AsyncConfig {
+    @Bean
+    fun taskExecutor(): ThreadPoolTaskExecutor {
+        val executor = ThreadPoolTaskExecutor()
+        executor.setThreadNamePrefix("async-executor-")
+        executor.corePoolSize = POOL_SIZE
+        executor.maxPoolSize = POOL_SIZE * 2
+        executor.queueCapacity = QUEUE_SIZE
+        executor.setWaitForTasksToCompleteOnShutdown(true)
+        executor.initialize()
+        return executor
+    }
+
+    companion object {
+        private const val POOL_SIZE = 3
+        private const val QUEUE_SIZE = 3
+    }
+}
