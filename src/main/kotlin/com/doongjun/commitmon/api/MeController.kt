00a5e41c0a593bcbafdbffe3d9bac3e21d130408
doongjun <dongjun.kim@purpleworks.co.kr>
2024-11-19 16:07:12 +0900

    #52 feat: Swagger 설명

--- src/main/kotlin/com/doongjun/commitmon/api/MeController.kt
+++ src/main/kotlin/com/doongjun/commitmon/api/MeController.kt
@@ -6,6 +6,7 @@ import com.doongjun.commitmon.api.data.MeResponse
 import com.doongjun.commitmon.app.UserFetchType
 import com.doongjun.commitmon.app.UserService
 import com.doongjun.commitmon.extension.currentUserId
+import io.swagger.v3.oas.annotations.Operation
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,12 +19,14 @@ import org.springframework.web.bind.annotation.RestController
 class MeController(
     private val userService: UserService,
 ) {
+    @Operation(summary = "로그인 유저 조회")
     @GetMapping
     fun get(): MeResponse =
         MeResponse.from(
             user = userService.getSimple(currentUserId!!),
         )
 
+    @Operation(summary = "로그인 유저 조회 (팔로워, 팔로잉 포함)")
     @GetMapping("/detail")
     fun getDetail(): MeDetailResponse =
         MeDetailResponse.from(
@@ -34,6 +37,7 @@ class MeController(
                 ),
         )
 
+    @Operation(summary = "로그인 유저 커밋몬 변경")
     @PostMapping("/commitmon/change")
     fun changeCommitmon(
         @Valid @RequestBody request: ChangeCommitmonRequest,
